45a531939b64098b6c4b3d017d1083a7060659e1
Milos <hi@beedev.dev>
2025-06-25 22:13:13 +0200

    Added node js

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,14 @@
+// middleware.ts
 import { type NextRequest } from 'next/server'
 import { updateSession } from '@/utils/supabase/middleware'
 
 export async function middleware(request: NextRequest) {
-    return await updateSession(request)
+  return await updateSession(request)
 }
 
 export const config = {
-    matcher: [
-      // isključuje statične fajlove i slike
-      '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
-    ],
-    runtime: 'edge', // eksplicitno postavljeno
-  }
+  runtime: 'nodejs', // ← bitno!
+  matcher: [
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+  ],
+}
